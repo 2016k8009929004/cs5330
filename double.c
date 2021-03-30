c9cf6d8da45d39e5815c411d7babab755701503a
2016k8009929004 <34846611+2016k8009929004@users.noreply.github.com>
2021-03-30 20:49:14 +0800

    Comment double tabulation

--- double.c
+++ double.c
@@ -10,28 +10,20 @@
  */
 
 #include <stdint.h>
+typedef unsigned __int128 uint128_t
 
 /**
- * @brief Simple Tabulation
+ * @brief Simple Tabulation to obtain (a) in Theorem 1
  * 
  * @param x 
  *    Key from Φ^c, c = 4
  * @param H 
- *    Tabulation from Φ = {0, ... , 2^8 - 1} -> Ψ = {0,1}, r = 32
+ *    Tabulation from Φ = {0, ... , 2^8 - 1} -> Ψ = {0, ... , 2^32 - 1}, d = 32
  * @return  
  *    Hash value of key x
  */
-uint32_t SimpleTab32(uint32_t x, uint32_t H[4][256]) {
-  /* H is pointing to randomly filled 4KB storage */
-  uint32_t i;
-  uint32_t h = 0;
-  uint8_t c;
-  for (i=0; i<4; i++) {
-    c = x;
-    h ^= H[i][c];
-    x = x >> 8;
-  }
-  return h;
+uint64_t * SimpleTab32(uint32_t x, uint32_t H[4][256]) {
+  
 }
 
 /**
