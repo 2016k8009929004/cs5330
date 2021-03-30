0dbc2be69e9ef72bf611658dafd616e577c81b36
2016k8009929004 <34846611+2016k8009929004@users.noreply.github.com>
2021-03-30 13:34:07 +0800

    Comment on simple tabulation

--- simple.c
+++ simple.c
@@ -11,7 +11,16 @@
 
 #include <stdint.h>
 
-/* Simple Tabulation algoritm */
+/**
+ * @brief Simple Tabulation
+ * 
+ * @param x 
+ *    Key from Φ^c, c = 4
+ * @param H 
+ *    Tabulation from Φ = {0, ... , 2^8 - 1} -> Ψ = {0,1}, r = 32
+ * @return  
+ *    Hash value of key x
+ */
 uint32_t SimpleTab32(uint32_t x, uint32_t H[4][256]) {
   /* H is pointing to randomly filled 4KB storage */
   uint32_t i;
