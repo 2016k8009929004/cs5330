d6eb41e9159161dc75474751cba25224aba7ad5b
2016k8009929004 <34846611+2016k8009929004@users.noreply.github.com>
2021-03-30 10:43:49 +0800

    update

--- twisted.c
+++ twisted.c
@@ -15,7 +15,7 @@
 uint32_t TwistedTab32(uint32_t x, uint64_t H[4][256]) {
   /* H is pointing to randomly filled 8KB storage */
   uint32_t i;
-  uint32_t h = 0;
+  uint64_t h = 0;
   uint8_t c;
   for (i=0; i<3; i++) {
     c = x;
