738dca59e14c65309c0558d638d05f7581ae4b41
Yuhang Chen <yuhangc@comp.nus.edu.sg>
2021-04-25 00:21:53 +0800

    add execution script

--- scripts/execute_applications.py
+++ scripts/execute_applications.py
@@ -17,12 +17,9 @@ def execute_binary(args):
     return rc, std_output.decode("utf-8"), std_error.decode("utf-8")
 
 
-def execute(binary_path, graph_path, execution_mode, sampling_method, source_type,
-            walk_num, length, thread_num, result_file):
+def execute(binary_path, result_file, first_para, second_para):
     # Execute the binary.
-    execution_args = generate_args(binary_path, '-f', graph_path, '-em', str(execution_mode),
-                                   '-sm', str(sampling_method), '-st', str(source_type),
-                                   '-wn', str(walk_num), '-l', str(length), '-n', str(thread_num), '-ew')
+    execution_args = generate_args(binary_path, first_para, second_para)
     print('Cmd: {0}'.format(' '.join(execution_args)))
 
     (rc, std_output, std_error) = execute_binary(execution_args)
@@ -38,154 +35,55 @@ def execute(binary_path, graph_path, execution_mode, sampling_method, source_typ
 
 
 # result file format:
-# binary: creeper, creeperN, homegrown
-# application: genericwalk, deepwalk, ppr, node2vec, metapath
-# execution mode: uniform, static, dynamic
-# sampling method: uniform, its, alias, rejection, maxweightrejection
-# source type: all, single, multiple
-# (binary)_(application)_(execution mode)_(sampling method)_(source type)_(num walks)_(length)_(thread).res
+# application: collision_check, linear_probing, cuckoo_hashing
+# (application)_(first parameters)_(second parameters).res
 
-application_names = ['genericwalk', 'deepwalk', 'ppr', 'node2vec', 'metapath']
-execution_mode_name = ['uniform', 'static', 'dynamic']
-sampling_method_name = ['uniform', 'its', 'alias', 'rejection', 'maxweightrejection']
-source_type_name = ['all', 'single', 'multiple']
+application_names = ['collision_check', 'linear_probing', 'cuckoo_hashing']
 
 
-# Execute creeperN
-# binary_name = 'creeperN'
-# build_type = 'cn_build'
+project_path = '/home/yuhang/Study/cs5330'
 
-# Execute creeper
-binary_name = 'creeper'
-build_type = 'creeper_build'
-
-
-project_path = '/home/yuhang/Work'
-dataset_path = '/data/graph_at_scale/dataset'
-result_path = '/data/graph_at_scale/creeper_results/logs'
-
-input_binary_path_format = project_path + '/XtraGraphComputing/{0}/random_walk/{1}.out'
-input_graph_path_format = dataset_path + '/{0}'
-input_result_path_format = result_path + '/{0}/{1}'
+input_binary_path_format = project_path + '/{0}/{0}_{1}.out'
+input_result_path_format = project_path + '/results/{0}'
 
 input_application_list = [
-   # 0, # genericwalk
-    1, # deepwalk
-    2, # ppr
-    3, # node2vec
-    4, # metapath
+    0,  # collision_check
+    1,  # linear_probing
+    2,  # cuckoo_hashing
 ]
 
-input_execution_mode_list = [
-    0, # uniform
-    1, # static
-    2, # dynamic
+hash_function_list = [
+    "simple",
+    "twisted",
+    "double"
 ]
 
-uniform_valid_sampling_list = [0]
-static_valid_sampling_list = [1, 2, 3]
-dynamic_valid_sampling_list = [1, 2, 3, 4]
-
-valid_sampling_list = [uniform_valid_sampling_list, static_valid_sampling_list, dynamic_valid_sampling_list]
-
-input_sampling_method_list = [
-    0, # uniform
-    1, # its,
-    2, # alias
-    3, # rejection
-    4, # maxweightrejection
+first_parameter_list = [
+    [10**3, 10**4, 10**5, 10**6, 10**7, 10**8],
+    [10**4, 10**5, 10**6, 10**7, 10**8],
+    [10**4, 10**5, 10**6, 10**7, 10**8],
 ]
 
-input_source_type_list = [
-    0, # all
-    1, # single
-    2, # multiple
+second_parameter_list = [
+    [0, 0, 0, 0, 0, 0],  #
+    [10**3, 10**4, 10**5, 10**6, 10**7],
+    [0.01, 0.01, 0.01, 0.01, 0.01, 0.01],
 ]
 
-input_length_list = [
-    # 5,
-    # 6,
-    80
-    # 160
-]
+for i, application in enumerate(application_names):
+    for hash_function in hash_function_list:
+        for j in range(len(first_parameter_list)):
+            first_para = first_parameter_list[i][j]
+            second_para = second_parameter_list[i][j]
+            input_binary_path = input_binary_path_format.format(application, hash_function)
+            input_result_path = input_result_path_format.format(application)
+            result_file_name = '{0}_{1}_{2}_{3}.res'.format(application, hash_function, first_para, second_para)
 
-input_walk_num_list = [
-    0
-]
+            if not os.path.exists(os.path.dirname(input_result_path)):
+                os.makedirs(os.path.dirname(input_result_path))
 
-input_graph_list = [
-    # 'eu2005',
-    # 'orkut',
-    # 'patents',
-    # 'uk2002',
-    # 'youtube',
-    # 'livejournal',
-    # 'twitter',
-    # 'friendster',
-    # 'amazon_book',
-    # 'amazon_clothing',
-    'wikidata',
-    'amazon',
-]
+            print(' '.join((input_binary_path, first_para, second_para)))
 
-input_thread_num_list = [
-    10
-]
+            execute(input_binary_path, input_result_path, first_para, second_para)
 
-# [execution mode, sampling method, source type, extend command]
-input_application_configuration_dict_list = [
-    {1: [1, 2, 0, ''], 2: [0, 0, 1, ''], 3: [2, 4, 0, ''], 4: [2, 1, 0, '-s 0,1,2,3,4 -el']},
-    # {0: [0, 0, 0, '']},
-    # {0: [1, 1, 0, '']},
-    # {0: [1, 2, 0, '']},
-    # {0: [1, 3, 0, '']},
-    # {0: [2, 4, 0, '']},
-]
-
-# (binary)_(application)_(execution mode)_(sampling method)_(source type)_(source_num)_(length)_(thread).res
-
-for input_graph in input_graph_list:
-    for input_application_configuration_dict in input_application_configuration_dict_list:
-        for input_application in input_application_list:
-            input_execution = input_application_configuration_dict[input_application][0]
-            input_sampling = input_application_configuration_dict[input_application][1]
-            input_source_type = input_application_configuration_dict[input_application][2]
-            input_cmd_extension = input_application_configuration_dict[input_application][3]
-
-            for input_walk_num in input_walk_num_list:
-                for input_length in input_length_list:
-                    for input_thread_num in input_thread_num_list:
-                        if input_application == 2:
-                            input_length = 5
-                        elif input_application == 4:
-                            input_length = 6
-                        result_file_name = '{0}_{1}_{2}_{3}_{4}_{5}_{6}_{7}.res'.format(binary_name,
-                                                                                        application_names[input_application],
-                                                                                        execution_mode_name[input_execution],
-                                                                                        sampling_method_name[input_sampling],
-                                                                                        source_type_name[input_source_type],
-                                                                                        input_walk_num,
-                                                                                        input_length,
-                                                                                        input_thread_num)
-
-                        input_binary_path = input_binary_path_format.format(build_type,
-                                                                            application_names[input_application])
-                        input_graph_path = input_graph_path_format.format(input_graph)
-                        input_result_path = input_result_path_format.format(input_graph, result_file_name)
-
-                        print(input_graph_path)
-                        print(input_result_path)
-                        print(input_binary_path)
-
-                        if os.path.exists(input_result_path):
-                            print('Result exists.')
-                            # continue
-
-                        if not os.path.exists(os.path.dirname(input_result_path)):
-                            os.makedirs(os.path.dirname(input_result_path))
-
-                        input_binary_path = ' '.join([input_binary_path, input_cmd_extension])
-                        execute(input_binary_path, input_graph_path, input_execution, input_sampling,
-                                input_source_type, input_walk_num, input_length, input_thread_num,
-                                input_result_path)
-                        print('-------------------------------------------------------------------------------')
+            print('-------------------------------------------------------------------------------')
